93d39b3eda156fc2ead76a27d98b92f361cac22f
Allegra <aukseallegra@gmail.com>
2018-07-15 23:35:43 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file.
+// returns all names lowercased, does not modify the original array:
+
+function lowerCaseDrivers(driversList){
+  return driversList.map(function (driver){
+    return driver.toLowerCase();
+  });
+}
+
+// returns list of objects with appropriate first and last names:
+
+function nameToAttributes(driversArray) {
+  return driversArray.map(function (driverName){
+
+    let driverFirstName = driverName.split(" ")[0];
+    let driverLastName = driverName.split(" ")[1];
+
+    return { firstName: driverFirstName, lastName: driverLastName };
+  });
+}
+
+// "<NAME OF DRIVER> is from <HOMETOWN>" :
+
+function attributesToPhrase(driversArray) {
+  return driversArray.map(function (driver){
+    return `${driver.name} is from ${driver.hometown}`;
+  })
+}
